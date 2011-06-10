cab473977117cbe6abf934ec1f45f799837aa1c0
rpoisel <rpoisel@76e0c08c-8e14-4548-9514-92ecd803f47c>
2011-06-10 12:12:04 +0000

    unittest added
    git-svn-id: https://showcase.fhstp.ac.at/republic/mmc/trunk@444 76e0c08c-8e14-4548-9514-92ecd803f47c

---
+++ lib/unittest_frags.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import unittest
+
+import frags
+
+class CTestFrags(unittest.TestCase):
+    def test_frags_and_headers(self):
+        lNumHeaders = 1000
+        lNumIter = 2
+        lFrags = frags.CFrags()
+        for lIter in xrange(lNumIter):
+            for lOffset in xrange(lNumHeaders):
+                lFrags.addHeader(lOffset)
+        self.assertEquals(len(lFrags.getHeaders()), len(lFrags.getBlocks()))
+        self.assertEquals(lFrags.getHeaders()[20], lFrags.getBlocks()[20])
+
+if __name__=="__main__":
+    unittest.main()
