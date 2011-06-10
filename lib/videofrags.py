feb6574333a7210f76fa1c4cd0ba40723a6ae4e3
rpoisel <rpoisel@76e0c08c-8e14-4548-9514-92ecd803f47c>
2011-06-10 05:21:02 +0000

    videofrags container added
    git-svn-id: https://showcase.fhstp.ac.at/republic/mmc/trunk@440 76e0c08c-8e14-4548-9514-92ecd803f47c

--- lib/videofrags.py
+++ lib/videofrags.py
@@ -5,49 +5,37 @@ class CVideoFrags:
         # offsets to relevant fragments
         self.__mFragments = []
 
-    def getHeaderIterator(self):
-        return CVideoFrags.CHeaderIterator(self.__mHeaders, self.__mFragments)
+    def getHeaders(self):
+        lHeaders = []
+        for lFragmentIdx in self.__mHeaders:
+            lHeaders.append(self.__mFragments[lFragmentIdx])
+        return lHeaders
 
-    def getFragmentIterator(self):
+    def getFragments(self):
         return self.__mFragments
 
     def addHeader(self, pHeaderOffset):
+        # determine if fragment exists
+        lIndex = 0
         try:
             lIndex = self.__mFragments.index(pHeaderOffset)
-            self.__mHeaders.append(lIndex)
         except ValueError:
             self.__mFragments.append(pHeaderOffset)
             lIndex = self.__mFragments.index(pHeaderOffset)
-            try:
-                lHeaderIdx = self.__mHeaders.index(lIndex)
-                return False
-            except ValueError:
-                self.__mHeaders.append(lIndex)
-                return True
+            self.__mHeaders.append(lIndex)
+            return True
+        # determine if index in headers exists
+        try:
+            lHdrIdx = self.__mHeaders.index(lIndex)
+        except ValueError:
+            self.__mHeaders.append(lIndex)
+            return True
+        return False
 
     def addFragment(self, pFragmentOffset):
-        # TODO check if fragment has already been added
         try:
             lIndex = self.__mFragments.index(pFragmentOffset)
             return False
         except ValueError:
             self.__mFragments.append(pFragmentOffset)
             return True
-
-    class CHeaderIterator:
-        # iterate through offsets that contain video headers
-        # resolve indeces in headers to offsets in fragments
-        def __init__(self, pHeaders, pFragments):
-            self.__mHeaders = pHeaders
-            self.__mFrags = pFragments
-            self.__mIdx = 0
-
-        def __iter__(self):
-            return self
-
-        def next(self):
-            if self.__mIdx >= len(self.__mHeaders):
-                raise StopIteration
-            lOffset = self.__mFrags[self.__mHeaders[self.__mIdx]]
-            self.__mIdx += 1
-            return lOffset
