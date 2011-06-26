631a010d200d2d4181df253ba7274cbf5726ae1c
rpoisel <rpoisel@76e0c08c-8e14-4548-9514-92ecd803f47c>
2011-06-26 23:44:09 +0000

    prototype of the filecarving gui.
    git-svn-id: https://showcase.fhstp.ac.at/republic/mmc/trunk@515 76e0c08c-8e14-4548-9514-92ecd803f47c

---
+++ gui/gui_fc.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+"""The user interface for our app"""
+
+
+import os
+import sys
+# PyQt4, PySide stuff
+from qtimport import *
+
+# Import the compiled UI module
+from file_carving_ui import Ui_MainFileCarving
+
+
+# Create a class for our main window
+class Gui_Qt(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        super(Gui_Qt, self).__init__(parent)
+        self.ui = Ui_MainFileCarving()
+        self.ui.setupUi(self)
+
+class CMain:
+    def __init__(self):
+        if gUsePyQt:
+            self.__mApp = QtGui.QApplication(sys.argv)
+        else:
+            # unicode issue work-around for PySide
+            self.__mApp = QtGui.QApplication("nothing")
+
+        self.__mWindow = Gui_Qt()
+
+    def run(self):
+        self.__mWindow.show()
+        lReturn = self.__mApp.exec_()
+        sys.exit(lReturn)
+
+
+def main():
+    lMain = CMain()
+    lMain.run()
+
+if __name__ == "__main__":
+    main()
---
+++ gui/qtimport.py
@@ -0,0 +1,9 @@
+
+gUsePyQt = False
+
+if gUsePyQt == True:
+	from PyQt4 import QtGui, QtCore
+else:
+	from PySide import QtGui, QtCore
+
+
