e541f8c95c52b8d2a6409c3f628763922a99c7de
rpoisel <rpoisel@76e0c08c-8e14-4548-9514-92ecd803f47c>
2011-08-03 08:11:45 +0000

    permutations improved
    git-svn-id: https://showcase.fhstp.ac.at/republic/mmc/trunk@558 76e0c08c-8e14-4548-9514-92ecd803f47c

--- reassembly/reassembly/reassembly_context.py
+++ reassembly/reassembly/reassembly_context.py
@@ -23,19 +23,21 @@ class CReassembly:
             if not os.path.exists(lDir):
                 os.makedirs(lDir)
             lRecoverData = ""
-            lFFMpeg = subprocess.Popen(
-                    ["ffmpeg", "-i", "-", lDir + "/" + pOptions.outputformat], 
-                    bufsize=512, stdin=subprocess.PIPE) #, stderr=subprocess.PIPE)
             lRecoverFH = open(pOptions.imagefile, "rb")
             lRecoverFH.seek(lFragHeader.mOffset, os.SEEK_SET)
             lRecoverHdrData = lRecoverFH.read(lFragHeader.mSize)
             for lCnt in xrange(len(lSortedFrags[lIdxNoHeader:])+1):
                 for lPermutation in itertools.permutations(lSortedFrags[lIdxNoHeader:], lCnt):
+                    print("Trying permutation: " + str(lFragHeader) + ' ' + \
+                            ''.join([str(lFrag)+' ' for lFrag in lPermutation]))
                     lRecoverData = lRecoverHdrData
                     for lFrag in lPermutation:
                         lRecoverFH.seek(lFrag.mOffset, os.SEEK_SET)
                         lRecoverData += lRecoverFH.read(lFrag.mSize)
-            lFFMpeg.communicate(input=lRecoverData)
+                    lFFMpeg = subprocess.Popen(
+                            ["ffmpeg", "-i", "-", lDir + "/" + pOptions.outputformat], 
+                            bufsize=512, stdin=subprocess.PIPE) #, stderr=subprocess.PIPE)
+                    lFFMpeg.communicate(input=lRecoverData)
             lRecoverFH.close()
             lCntHdr += 1
             pCaller.progressCallback(100 * len(lSortedFrags[0:lIdxNoHeader]) / (lCntHdr))
