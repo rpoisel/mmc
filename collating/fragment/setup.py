5c281af080abf890e1003a7c55a6acac31bed78e
rpoisel <rpoisel@76e0c08c-8e14-4548-9514-92ecd803f47c>
2011-09-24 11:23:22 +0000

    setup improved for the fragment classifier
    git-svn-id: https://showcase.fhstp.ac.at/republic/mmc/trunk@690 76e0c08c-8e14-4548-9514-92ecd803f47c

--- collating/fragment/setup.py
+++ collating/fragment/setup.py
@@ -1,11 +1,24 @@
+import os
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 setup(
+        name="Fragment Classifier",
         cmdclass={'build_ext' : build_ext},
-        ext_modules=[Extension("fragment_context",
-            ["fragment_context.pyx"],
+        #ext_package='fragment_classifier',
+        ext_modules=[
+            Extension(
+            name="libfragment_classifier",
+            sources=["src" + os.sep + "fragment_classifier.c",
+                "src" + os.sep + "ncd.c"],
+            libraries=["z"],
+            library_dirs=["."],
+            include_dirs=["include"]),
+            Extension(
+            name="fragment_context",
+            sources=["fragment_context.pyx"],
             libraries=["fragment_classifier"],
+            library_dirs=["."],
             include_dirs=["."])]
 )
