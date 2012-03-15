f1b4b7991d3d917234d024fe6e6b46d55a007a1a
rpoisel <rpoisel@76e0c08c-8e14-4548-9514-92ecd803f47c>
2012-03-15 19:04:18 +0000

    library support improved for setup.py
    git-svn-id: https://showcase.fhstp.ac.at/republic/mmc/trunk@800 76e0c08c-8e14-4548-9514-92ecd803f47c

--- setup.py
+++ setup.py
@@ -17,9 +17,10 @@ lTarget = Executable(
     includes=["PySide.QtCore", "PySide.QtGui", "PySide.QtXml", "magic"]
     )
 
+include_files = []
 if platform.system().lower() == "linux":
-    include_files = [("collating/fragment/libfragment_classifier.so", \
-            "libfragment_classifier.so")]
+    include_files.append(("collating/fragment/libfragment_classifier.so", \
+            "libfragment_classifier.so"))
 pathAdd(include_files, os.path.join('data', 'magic'), ".mgc")
 pathAdd(include_files, 'data', ".img")
 
