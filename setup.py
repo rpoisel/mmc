08c492f71944fa0eba014981b6aec1d79ce20e4d
rpoisel <rpoisel@76e0c08c-8e14-4548-9514-92ecd803f47c>
2012-03-15 12:52:30 +0000

    necessary data files included
    git-svn-id: https://showcase.fhstp.ac.at/republic/mmc/trunk@790 76e0c08c-8e14-4548-9514-92ecd803f47c

--- setup.py
+++ setup.py
@@ -3,22 +3,35 @@ import os
 
 from cx_Freeze import setup, Executable
 
+
+def pathAdd(pList, pPath, pExtension):
+    for lFile in os.listdir(pPath):
+        lFilePath = os.path.join(pPath, lFile)
+        if os.path.isfile(lFilePath) \
+                and lFile.endswith(pExtension):
+            pList.append(lFilePath)
+
 lTarget = Executable(
-    script = "gui/gui_fc.py",
-    includes = ["PySide.QtCore","PySide.QtGui","PySide.QtXml", "magic"]
-    ) 
-include_files = [("collating/fragment/libfragment_classifier.so", "libfragment_classifier.so")]
+    script="gui/gui_fc.py",
+    includes=["PySide.QtCore", "PySide.QtGui", "PySide.QtXml", "magic"]
+    )
+
+include_files = [("collating/fragment/libfragment_classifier.so", \
+        "libfragment_classifier.so")]
+pathAdd(include_files, os.path.join('data', 'magic'), ".mgc")
+pathAdd(include_files, 'data', ".img")
+
 setup(
-    version = "0.9.5",
-    description = "Multimedia File Carver",
-    author = "Rainer Poisel",
-    author_email = "rainer.poisel@fhstp.ac.at",
-    name = "mmc",
-    options = {"build_exe": {
+    version="0.9.5",
+    description="Multimedia File Carver",
+    author="Rainer Poisel",
+    author_email="rainer.poisel@fhstp.ac.at",
+    name="mmc",
+    options={"build_exe": {
                              "append_script_to_exe": True,
                              "create_shared_zip": False,
                              "include_files": include_files,
                             }
                },
-    executables = [lTarget]
+    executables=[lTarget]
     )
