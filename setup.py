3cca352e2410d918a06d2b8dff5bc2457eb99adf
rpoisel <rpoisel@76e0c08c-8e14-4548-9514-92ecd803f47c>
2012-03-15 19:03:04 +0000

    setup.py updated for better support in windows
    git-svn-id: https://showcase.fhstp.ac.at/republic/mmc/trunk@799 76e0c08c-8e14-4548-9514-92ecd803f47c

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import platform
 import sys
 import os
 
@@ -16,8 +17,9 @@ lTarget = Executable(
     includes=["PySide.QtCore", "PySide.QtGui", "PySide.QtXml", "magic"]
     )
 
-include_files = [("collating/fragment/libfragment_classifier.so", \
-        "libfragment_classifier.so")]
+if platform.system().lower() == "linux":
+    include_files = [("collating/fragment/libfragment_classifier.so", \
+            "libfragment_classifier.so")]
 pathAdd(include_files, os.path.join('data', 'magic'), ".mgc")
 pathAdd(include_files, 'data', ".img")
 
