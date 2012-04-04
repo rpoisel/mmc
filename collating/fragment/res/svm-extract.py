4eb67b698aa0d0a2d80560283433d686744fddf6
rpoisel <rpoisel@76e0c08c-8e14-4548-9514-92ecd803f47c>
2012-04-04 07:52:28 +0000

    recursive feature extraction supported
    git-svn-id: https://showcase.fhstp.ac.at/republic/mmc/trunk@866 76e0c08c-8e14-4548-9514-92ecd803f47c

--- collating/fragment/res/svm-extract.py
+++ collating/fragment/res/svm-extract.py
@@ -9,27 +9,36 @@ from fragment_context import FileType
 
 
 def main():
-    lFilename = sys.argv[1]
+    if len(sys.argv) != 3:
+        print "Usage: " + sys.argv[0] + " <path> <block-size>"
+        sys.exit(-1)
+    lPath = sys.argv[1]
     lBlockSize = int(sys.argv[2])
-    with open(lFilename, "rb") as lFH:
-        # continue over the first block
-        lFH.seek(lBlockSize, os.SEEK_SET)
-        lBlock = lFH.read(lBlockSize)
-        while lBlock != "" and len(lBlock) == lBlockSize:
-            # calc BFD
-            lBFD = [0 for lCnt in xrange(256)]
-            for lCnt in xrange(lBlockSize):
-                lBFD[ord(lBlock[lCnt])] += 1
-            if lFilename.lower().endswith(".jpg"):
-                lFeatureStr = str(FileType.FT_JPG)
-            elif lFilename.lower().endswith(".png"):
-                lFeatureStr = str(FileType.FT_PNG)
-            else:
-                lFeatureStr = str(FileType.FT_UNKNOWN)
-            for lCnt in xrange(256):
-                lFeatureStr += " " + str(lCnt) + ":" + str(lBFD[lCnt])
-            print lFeatureStr
-            lBlock = lFH.read(lBlockSize)
+    try:
+        for lRoot, lDirs, lFiles in os.walk(lPath):
+            for lFile in lFiles:
+                with open(os.path.join(lRoot, lFile), "rb") as lFH:
+                    # continue over the first block
+                    lFH.seek(lBlockSize, os.SEEK_SET)
+                    lBlock = lFH.read(lBlockSize)
+                    while lBlock != "" and len(lBlock) == lBlockSize:
+                        # calc BFD
+                        lBFD = [0 for lCnt in xrange(256)]
+                        for lCnt in xrange(lBlockSize):
+                            lBFD[ord(lBlock[lCnt])] += 1
+                        if lFile.lower().endswith(".jpg"):
+                            lFeatureStr = str(FileType.FT_JPG)
+                        elif lFile.lower().endswith(".png"):
+                            lFeatureStr = str(FileType.FT_PNG)
+                        else:
+                            lFeatureStr = str(FileType.FT_UNKNOWN)
+                        for lCnt in xrange(256):
+                            lFeatureStr += " " + str(lCnt) + ":" + \
+                                    str(lBFD[lCnt])
+                        print lFeatureStr
+                        lBlock = lFH.read(lBlockSize)
+    except Exception, pExc:
+        print "Error: " + str(pExc)
 
 if __name__ == "__main__":
     main()
