ce0fecdef4aa51883daa694db1fcd821b771e82d
rpoisel <rpoisel@76e0c08c-8e14-4548-9514-92ecd803f47c>
2012-04-06 16:30:38 +0000

    range reading method added
    git-svn-id: https://showcase.fhstp.ac.at/republic/mmc/trunk@874 76e0c08c-8e14-4548-9514-92ecd803f47c

--- collating/fragment/res/svm-test.py
+++ collating/fragment/res/svm-test.py
@@ -7,17 +7,27 @@ import svmutil
 
 import copy
 
-def scale_values(pX, pLower, pUpper):
+def read_ranges(pPathRange):
+    lReturn = {}
+    lFH = open(pPathRange, "r")
+    for lLine in lFH:
+        lValues = lLine.split()
+        if len(lValues) == 3:
+            lReturn[int(lValues[0])] = (int(lValues[1]), int(lValues[2]))
+    lFH.close()
+    return lReturn
+
+def scale_values(pX, pLower, pUpper, pRanges):
     # TODO: WRONG, scale to range file which contains the 
     #       min/max values of features
 
     # do this for the first feature only
     for lDict in pX:
-        print "Dict: " + str(lDict)
+        #print "Dict: " + str(lDict)
         lMin = lDict[min(lDict, key=lambda x : lDict[x])]
-        print "Min: " + str(lMin)
+        #print "Min: " + str(lMin)
         lMax = lDict[max(lDict, key=lambda x : lDict[x])]
-        print "Max: " + str(lMax)
+        #print "Max: " + str(lMax)
         for lKey in lDict.keys():
             if lDict[lKey] == lMin:
                 lDict[lKey] = pLower
@@ -32,14 +42,14 @@ def scale_values(pX, pLower, pUpper):
 def main():
     try:
         lModel = svmutil.svm_load_model(sys.argv[1])
-        print lModel
-        lY, lX = svmutil.svm_read_problem(sys.argv[2])
-        scale_values(lX, -1., 1.)
-        #lX = normalize_instances(lX)
-        print svmutil.svm_predict(lY, lX, lModel)[0]
+        lY, lX = svmutil.svm_read_problem(sys.argv[3])
+        lRanges = read_ranges(sys.argv[2])
+        scale_values(lX, -1., 1., lRanges)
+        lPredict = svmutil.svm_predict(lY, lX, lModel)[0]
+        #print svmutil.svm_predict(lY, lX, lModel)[0]
         #print lX
     except IndexError, pExc:
-        print "Usage: " + sys.argv[0] + " <model-file> <problem-file>"
+        print "Usage: " + sys.argv[0] + " <model-file> <range-file> <problem-file>"
 
 if __name__ == "__main__":
     main()
