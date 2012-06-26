3ec0115d5ece0fffef883926b75c81cd123d5db6
rpoisel <rpoisel@76e0c08c-8e14-4548-9514-92ecd803f47c>
2012-06-26 21:18:01 +0000

    better handling of dlls/shared objects
    git-svn-id: https://showcase.fhstp.ac.at/republic/mmc/trunk@935 76e0c08c-8e14-4548-9514-92ecd803f47c

--- collating/fragment/vars.mk
+++ collating/fragment/vars.mk
@@ -2,15 +2,23 @@ SRC_DIR=src
 BUILD_DIR=build
 OUT_DIR=.
 
-# ================ START =================
+LBITS := $(shell getconf LONG_BIT)
+
+# ================ START FRAGMENT CLASSIFIER =================
 BIN_FRAGMENT_CLASSIFIER=$(OUT_DIR)/libfragment_classifier.so
 OBJ_FRAGMENT_CLASSIFIER= $(BUILD_DIR)/fragment_classifier.o \
 			 $(BUILD_DIR)/entropy/entropy.o
 CFLAGS_FRAGMENT_CLASSIFIER=$(CFLAGS) -fPIC -Iinclude/entropy
-LDFLAGS_FRAGMENT_CLASSIFIER=-shared -Wl,-soname, -lm -lpthread -lmagic #-lsvm
-# ================= END ==================
+ifeq ($(LBITS),64)
+    LDFLAGS_FRAGMENT_CLASSIFIER=-shared -Wl,-soname, -lm \
+	-lpthread -Llib/magic/linux-x86_64 -lmagic #-lsvm
+else
+    LDFLAGS_FRAGMENT_CLASSIFIER=-shared -Wl,-soname, -lm \
+	-lpthread -Llib/magic/linux-i686 -lmagic #-lsvm
+endif
+# ================= END FRAGMENT CLASSIFIER ==================
 
-# ================ START =================
+# ================ START BLOCK READER =================
 BIN_BLOCK_READER=$(OUT_DIR)/libblock_reader.so
 OBJ_BLOCK_READER= $(BUILD_DIR)/block_reader.o \
 		  $(BUILD_DIR)/fragment_collection.o
@@ -18,14 +26,14 @@ CFLAGS_BLOCK_READER=$(CFLAGS) -fPIC
 LDFLAGS_BLOCK_READER=-shared -Wl,-soname, -L. \
 		     -lfragment_classifier \
 		     -lblock_collection
-# ================= END ==================
+# ================= END BLOCK READER ==================
 
-# ================ START =================
+# ================ START BLOCK COLLECTION =================
 BIN_BLOCK_COLLECTION=$(OUT_DIR)/libblock_collection.so
 OBJ_BLOCK_COLLECTION= $(BUILD_DIR)/block_collection.o
 CFLAGS_BLOCK_COLLECTION=$(CFLAGS) -fPIC
 LDFLAGS_BLOCK_COLLECTION=-shared -Wl,-soname,
-# ================= END ==================
+# ================= END BLOCK COLLECTION ==================
 
 # ================ START DATA SNIFFER =================
 LDFLAGS_DATA_SNIFFER=-L. -lfragment_classifier
