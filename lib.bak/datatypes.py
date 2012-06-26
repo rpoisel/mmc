5d1ce0786b5080b99f59d1375d6f952cb9f8b6a9
rpoisel <rpoisel@76e0c08c-8e14-4548-9514-92ecd803f47c>
2012-06-26 21:18:10 +0000

    conflict resolved by removing unnecessary files
    git-svn-id: https://showcase.fhstp.ac.at/republic/mmc/trunk@939 76e0c08c-8e14-4548-9514-92ecd803f47c

--- lib.bak/datatypes.py
+++
@@ -1,7 +0,0 @@
-# http://stackoverflow.com/questions/36932/\
-#        whats-the-best-way-to-implement-an-enum-in-python
-
-
-def enum(*sequential, **named):
-    enums = dict(zip(sequential, range(len(sequential))), **named)
-    return type('Enum', (), enums)
