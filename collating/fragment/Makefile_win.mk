f0c3773b60ccb17179ba890bf54d2d7364e3df6e
Rainer Poisel <rainer.poisel@gmail.com>
2012-08-25 18:51:01 +0200

    windows makefile improved

--- collating/fragment/Makefile_win.mk
+++ collating/fragment/Makefile_win.mk
@@ -8,10 +8,10 @@ FRAGMENT_CLASSIFIER=libfragment_classifier
 # ================= END ==================
 
 $(FRAGMENT_CLASSIFIER).dll: 
-    cl /c src\fragment_classifier.c /Iinclude /Iinclude\pthreads /Iinclude\magic
-    cl /c src\entropy\entropy.c /Iinclude\entropy /Iinclude
-    link fragment_classifier.obj entropy.obj /DLL /out:$(FRAGMENT_CLASSIFIER).dll
-              
+	cl /c src\fragment_classifier.c /Iinclude /Iinclude\pthreads /Iinclude\magic
+	cl /c src\entropy\entropy.c /Iinclude\entropy /Iinclude
+	link fragment_classifier.obj entropy.obj /DLL /out:$(FRAGMENT_CLASSIFIER).dll
+
 # ================ START =================
 BLOCK_COLLECTION=libblock_collection
 #CFLAGS_BLOCK_COLLECTION=$(CFLAGS) -fPIC
@@ -21,7 +21,7 @@ BLOCK_COLLECTION=libblock_collection
 $(BLOCK_COLLECTION).dll:
 	cl /c src\block_collection.c /Iinclude
 	link block_collection.obj /DLL /out:$(BLOCK_COLLECTION).dll
-	
+
 # ================ START =================
 BLOCK_READER=libblock_reader
 #CFLAGS_BLOCK_READER=$(CFLAGS) -fPIC
@@ -33,7 +33,6 @@ $(BLOCK_READER).dll: $(BLOCK_COLLECTION).dll $(FRAGMENT_CLASSIFIER).dll
 	cl /c src\fragment_collection.c /Iinclude
 	link $(BLOCK_COLLECTION).lib $(FRAGMENT_CLASSIFIER).lib block_reader.obj fragment_collection.obj /DLL /out:$(BLOCK_READER).dll
 
-
 # ================ START DATA SNIFFER =================
 LDFLAGS_DATA_SNIFFER=-L. -lfragment_classifier
 OBJ_DATA_SNIFFER=$(BUILD_DIR)/data_sniffer.o \
@@ -42,6 +41,6 @@ BIN_DATA_SNIFFER=$(OUT_DIR)/data_sniffer
 # ================= END DATA SNIFFER ==================
 
 all: $(BLOCK_READER).dll
-    
+
 clean:
-    del $(FRAGMENT_CLASSIFIER).* $(BLOCK_READER).* $(BLOCK_COLLECTION).* *.obj
+	del $(FRAGMENT_CLASSIFIER).* $(BLOCK_READER).* $(BLOCK_COLLECTION).* *.obj
