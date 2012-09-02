c2d00edbcd6fc2805604117d80049a20c56fa2d7
Rainer Poisel <rainer.poisel@gmail.com>
2012-09-02 16:48:52 +0200

    data_sniffer changed to stand-alone program instead of program using a library

--- collating/fragment/vars.mk
+++ collating/fragment/vars.mk
@@ -27,9 +27,21 @@ endif
 # ================= END FRAGMENT CLASSIFIER ==================
 
 # ================ START DATA SNIFFER =================
-LDFLAGS_DATA_SNIFFER=-L. -lfragment_classifier
+LDFLAGS_DATA_SNIFFER=
+ifeq ($(LBITS),64)
+    LDFLAGS_DATA_SNIFFER=-lm \
+	-lpthread -Llib/magic/linux-x86_64 -lmagic #-lsvm
+else
+    LDFLAGS_DATA_SNIFFER=-lm \
+	-lpthread -Llib/magic/linux-i686 -lmagic #-lsvm
+endif
 OBJ_DATA_SNIFFER=$(BUILD_DIR)/data_sniffer.o \
-		 $(BUILD_DIR)/block_collection.o
-BIN_DATA_SNIFFER=$(OUT_DIR)/data_sniffer.bin
+		 $(BUILD_DIR)/logging.o \
+		 $(BUILD_DIR)/fragment_classifier.o \
+		 $(BUILD_DIR)/fragment_collection.o \
+		 $(BUILD_DIR)/block_collection.o \
+		 $(BUILD_DIR)/entropy/entropy.o \
+
+BIN_DATA_SNIFFER=$(OUT_DIR)/data_sniffer
 # ================= END DATA SNIFFER ==================
 
